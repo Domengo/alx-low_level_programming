e0996d0f1a199075f7db1a0c00c4548bb83b8704
Domengo <dominicsengo@gmail.com>
2022-07-01 00:40:52 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ long long llong;
 float d;
 
 printf("Size of char: %lu byte(s)\n", sizeof(ch));
-printf("size of an int: %lu byte(s)\n" , sizeof(in));
+printf("size of an int: %lu byte(s)\n", sizeof(in));
 printf("Size of a long int: %lu byte(s)\n", sizeof(lng));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(llong));
 printf("Size of a float: %lu byte(s)\n", sizeof(d));
