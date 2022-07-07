dd983b7e5826ac8340abb0598daf7e10249ba51e
Domengo <dominicsengo@gmail.com>
2022-07-07 21:03:16 +0000

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * idigit checks for digits 1-9
+ * @c: character to be checked
+ * Return : 1 if c is a digit, 0 if a number
+ */
+
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+        return (1);
+    else
+        return (0);
+}
