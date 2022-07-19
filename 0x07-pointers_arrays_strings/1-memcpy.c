ad0d266fd20b61547f1a384a0eef06caeb5820fc
Domengo <dominicsengo@gmail.com>
2022-07-19 11:31:54 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memcpy copies n bytes from source(src) to a destination(dest)
+ * returns a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
