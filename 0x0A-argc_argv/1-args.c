1c0e3fd12d2709ca94246e95f39ca1d3e2d0694e
Domengo <dominicsengo@gmail.com>
2022-07-22 20:22:32 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints out all of the arguments passed to it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+*argv = *argv;
+
+printf("argc = %d\n", argc - 1);
+
+return (0);
+}
