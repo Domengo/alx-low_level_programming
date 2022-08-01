e0f053435d408eab058eb783eee863e8de120397
Domengo <dominicsengo@gmail.com>
2022-08-01 14:05:25 +0000

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
- * struct dog - my main struct
+/**
+ * struct dog - a dog's basic info
  * @name: first member
  * @age : second member
  * @owner : Third member
  *
  * has no return value it
  */
-
 struct dog
 {
 	char *name;
@@ -22,7 +21,7 @@ struct dog
  */
 typedef struct dog dog_t;
 
-/*
+/**
 *void init_dog(struct dog *d, char *name, float age, char *owner);
 *void print_dog(struct dog *d);
 *dog_t *new_dog(char *name, float age, char *owner);
