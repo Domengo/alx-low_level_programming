e0bc2a401889d3aee397d06c1420d1d0cd8e92f7
Domengo <dominicsengo@gmail.com>
2022-08-02 15:11:01 +0000

    file name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* __FILE__ prints the current file
+*
+*/
+
+int main (void)
+{
+	printf("%s \n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file
+* __FILE__ prints the current file
+*main - checks the code
+*Return: Always 0(success)
+*/
+
+int main(void)
+{
+	printf("%s \n", __FILE__);
+	return (0);
+}
