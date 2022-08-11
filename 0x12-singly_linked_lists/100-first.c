68b7ae4baa8a27b55fc296a051efd732594354e6
Domengo <dominicsengo@gmail.com>
2022-08-11 21:44:07 +0000

    shell shocked

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -15,5 +15,5 @@ void print (void) __attribute__ ((constructor));
 void print(void)
 {
 	printf("You're beat! and yet, you must allow,");
-	printf("\n bore my house upon my back!\n");
+	printf("\nI bore my house upon my back!\n");
 }
