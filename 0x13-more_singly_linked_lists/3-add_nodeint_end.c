bcf41d7b62aba484c783eb7c41049b9030cd8307
Domengo <dominicsengo@gmail.com>
2022-08-16 20:38:34 +0000

    add node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
- * @head: pointer to the last node in the list
+ * add_nodeint_end - adds a new node at the beginning of a linked list
+ * @head: pointer to the first element in the list
  * @n: data to insert in that new node
  *
  * Return: pointer to the new node, or NULL if it fails
