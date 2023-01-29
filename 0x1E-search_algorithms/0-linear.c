9f383691e103597502fb4127b3061d20fb5713a8
Domengo <dominicsengo@gmail.com>
2023-01-30 00:31:54 +0300

    search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <stdio.h>
 /**
  * linear_search - function to search value
  * array - pointer to the first element of the array to search in
@@ -11,6 +12,7 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	for (i = 0; i < size; i++)
+	printf("Value checked array[%lu] = [%d]\n", i, value);
 		if (array[i] == value)
 		return (i);
 	return (-1);
