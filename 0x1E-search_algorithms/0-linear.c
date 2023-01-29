6a0f620f6455d19b09a1ada42a60b2dbdafe16ea
Domengo <dominicsengo@gmail.com>
2023-01-30 00:15:01 +0300

    search algo

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+/**
+ * linear_search - function to search value
+ * array - pointer to the first element of the array to search in
+ * size - number of elements in the array
+ * value - value to be searched
+ * Returns value if found and -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		if (array[i] == value)
+		return (i);
+	return (-1);
+}
